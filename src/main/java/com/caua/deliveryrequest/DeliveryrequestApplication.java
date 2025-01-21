82297742ac6d637b0582447a39e2f8c2e85c87af
Cauã Pereira <cauapereira_@hotmail.com>
2025-01-21 17:59:36 -0300

    Workspace setted up

---
+++ src/main/java/com/caua/deliveryrequest/DeliveryrequestApplication.java
@@ -0,0 +1,13 @@
+package com.caua.deliveryrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryrequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryrequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caua/deliveryrequest/DeliveryrequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caua.deliveryrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryrequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
